259df72505a6e032f28c0b30d8a2f3e6e71c9afc
RuthMaria <ruth-15rms@hotmail.com>
2022-06-03 16:54:39 -0300

    feat: remove autofill input on chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,12 +5,23 @@ import Styles from './input-styles.scss'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={enableInput}/>
       <span className={Styles.status}>🔴</span>
     </div>
   )
 }
 
 export default Input
+
+/*
+readOnly => faz com que o input seja de somente leitura, não podendo ser alterado/digitado
+onFocus => quando o input estiver com foco um evento é lançado para setar o readOnly como falso, permitindo a digitação do campo.
+
+Esses dois hacks faz com que o navegador não use o auto-completar, evitando o preenchimento automático dos inputs quando o login estiver salvo no navegador
+*/
