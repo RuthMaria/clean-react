14f7181a5c5d55f7df6a207c12ca1edc5f73d7cc
RuthMaria <ruth-15rms@hotmail.com>
2022-06-05 18:14:45 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
-import { ValidationSpy } from '@/presentation/pages/login/login.spec'
+import { ValidationSpy } from '@/presentation/test'
 
 const Router: React.FC = () => {
   const validationSpy = new ValidationSpy()
