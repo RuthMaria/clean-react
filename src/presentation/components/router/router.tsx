bd5c068a076f9aa4b3d3550f55b219827bb78940
RuthMaria <ruth-15rms@hotmail.com>
2022-06-05 16:10:35 -0300

    feat: ensure login calls validation with correct email

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
+import { ValidationSpy } from '@/presentation/pages/login/login.spec'
 
 const Router: React.FC = () => {
+  const validationSpy = new ValidationSpy()
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<Login validation={validationSpy}/>} />
       </Routes>
     </BrowserRouter>
   )
